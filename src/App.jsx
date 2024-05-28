4c1a66d0b7bdee9a16cec659c856720b12bb2c63
Jeevan Poudel <jeevan1.785401@cct.tu.edu.np>
2024-05-28 15:21:21 +0545

    Adding frontend content

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import Hero from './component/Hero'
 import './App.css'
+const[iscompletestate,useiscompletestate]=useState(false);
 const App = () => {
   return (
+    
     <>
     <div className="hero">
 
    <div className="app">
-    <h3>My Todo</h3>
+    <h1>My Todo</h1>
 
     <div className="todo-wrapper">
       <div className="todo-input">
         <div className="todo-input-items">
-          <label htmlFor="">Title</label>
+          <label htmlFor="">Title : </label>
           <input type="text" name="Todays Todo" placeholder="Today's task Todo" />
         </div>
 
         <div className="todo-input-items">
-          <label htmlFor="">Discription</label>
+          <label htmlFor="">Discription : </label>
           <input type="text" name="Todays Todo disc" placeholder="Today's task Todo Discription" />
         </div>
 
         <div className="todo-input-items">
-        <input type="button" value="Add" className='primarybtn' />
+<input type="button" className="primarybtn" value="Add" placeholder='Add' />
 
         </div>
       </div>
 
     <div className="btn-area">
-      <button>Todo</button>
-      <button>Completed</button>
+      <button  className={`iscompletestate ${iscompletestate === false && `active`}`} onClick={()=>{setiscompletestate(false)}}>Todo</button>
+      <button className={`iscompletestate ${iscompletestate === true && `active`}`} onClick={()=>{setiscompletestate(true)}}>Completed</button>
     </div>
 
     <div className="todo-list">
